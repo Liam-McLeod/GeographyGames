da872562d3e8a6552c18c9b991fe64d7ebe3d7ff
Liam-McLeod <liam.mcleod42@gmail.com>
2023-03-24 18:18:11 -0400

    Added Zoom to Find Country and World Map

--- js/WorldMap.js
+++ js/WorldMap.js
@@ -1,3 +1,87 @@
+function zoomEffect() {
+    const NF = 16, 
+			NAV_MAP = {
+				187: { dir:  1, act: 'zoom', name: 'in' } /* + */, 
+				 61: { dir:  1, act: 'zoom', name: 'in' } /* + WTF, FF? */, 
+				189: { dir: -1, act: 'zoom', name: 'out' } /* - */, 
+				173: { dir: -1, act: 'zoom', name: 'out' } /* - WTF, FF? */, 
+				 37: { dir: -1, act: 'move', name: 'left', axis: 0 } /* ⇦ */, 
+				 38: { dir: -1, act: 'move', name: 'up', axis: 1 } /* ⇧ */, 
+				 39: { dir:  1, act: 'move', name: 'right', axis: 0 } /* ⇨ */, 
+				 40: { dir:  1, act: 'move', name: 'down', axis: 1 } /* ⇩ */
+			}, 
+			_SVG = document.querySelector('svg'), 
+			VB = _SVG.getAttribute('viewBox').split(' ').map(c => +c), 
+			DMAX = VB.slice(2), WMIN = 8;
+
+    let rID = null, f = 0, nav = {}, tg = Array(4);
+
+    function stopAni() {
+        cancelAnimationFrame(rID);
+        rID = null;  
+    };
+
+    function update() {	
+        let k = ++f/NF, j = 1 - k, cvb = VB.slice();
+
+        if(nav.act === 'zoom') {		
+            for(let i = 0; i < 4; i++)
+                cvb[i] = j*VB[i] + k*tg[i]
+        }
+
+        if(nav.act === 'move')	 {
+            cvb[nav.axis] = j*VB[nav.axis] + k*tg[nav.axis];
+        }
+        _SVG.setAttribute('viewBox', cvb.join(' '));
+
+        if(!(f%NF)) {
+            f = 0;
+            VB.splice(0, 4, ...cvb);
+            nav = {};
+            tg = Array(4);
+            stopAni();
+            return;
+        }
+        rID = requestAnimationFrame(update)
+    }
+
+    addEventListener('keydown', e => { e.preventDefault() }, false);
+    addEventListener('keypress', e => { e.preventDefault() }, false);
+
+    addEventListener('keyup', e => {
+        e.preventDefault();
+            
+        if(!rID && e.keyCode in NAV_MAP) {
+            nav = NAV_MAP[e.keyCode];
+            
+            if(nav.act === 'zoom') {
+                if((nav.dir === -1 && VB[2] >= DMAX[0]) || 
+                    (nav.dir ===  1 && VB[2] <= WMIN)) {
+                    //console.log(`cannot ${nav.act} ${nav.name} more`);
+                    return
+                }
+                
+                for(let i = 0; i < 2; i++) {
+                    tg[i + 2] = VB[i + 2]/Math.pow(2, nav.dir);
+                    tg[i] = .5*(DMAX[i] - tg[i + 2]);
+                }
+            }
+            
+            else if(nav.act === 'move') {
+                if((nav.dir === -1 && VB[nav.axis] <= 0) || 
+                    (nav.dir ===  1 && VB[nav.axis] >= DMAX[nav.axis] - VB[2 + nav.axis])) {
+                    //console.log(`at the edge, cannot go ${nav.name}`);
+                    return
+                }
+                
+                tg[nav.axis] = VB[nav.axis] + .5*nav.dir*VB[2 + nav.axis];
+            }
+            
+            update()
+        }
+    }, false);
+}
+
 async function getData(place) {
     
     const url = `https://restcountries.com/v3.1/name/${place}?fullText=true`;
@@ -30,37 +114,35 @@ async function getData(place) {
     document.getElementById("currency").innerText = `Currency: ${currencyName} (${currencySymbol} ${currencyAbb})`;
 }
 
+var countries = document.querySelectorAll("path");
+var countryList = Array.from(countries);
 
-var svgObject = document.getElementById('mySVG');
-// Embedding SVG Image
-svgObject.onload = function() {
+// Event Listeners
+// Loop through all path elements, add 3 events to each path
+countryList.forEach(elem=>{
 
-    // Use to get SVG content
-    var svgDoc = svgObject.contentDocument;
-
-    // Loop through all path elements, add 3 events to each path
-    svgDoc.querySelectorAll("path").forEach(elem=>{
-
-        elem.addEventListener("mouseover",function(){
-            // Get all elements with same name as current element
-            list = svgDoc.getElementsByName(elem.getAttribute("name"))
-            for(let i = 0;i<list.length;i++) {
-                list[i].style.fill = 'darkgrey';
-            }
-        })
-
-        elem.addEventListener("mouseleave",function() {
-            // Get all elements with same name as current element
-            list = svgDoc.getElementsByName(elem.getAttribute("name"))
-            for(let i = 0;i<list.length;i++) {
-                list[i].style.fill = '#ececec';
-            }
-        })
-
-        elem.addEventListener("click",function(event){
-            // API Call
-            getData(elem.getAttribute("name"));
-        })
-
+    elem.addEventListener("mouseover",function(){
+        // Get all elements with same name as current element
+        list = document.getElementsByName(elem.getAttribute("name"));
+        for(let i = 0;i<list.length;i++) {
+            list[i].style.fill = 'darkgrey';
+        }
     })
-}
+
+    elem.addEventListener("mouseleave",function() {
+        // Get all elements with same name as current element
+        list = document.getElementsByName(elem.getAttribute("name"));
+        for(let i = 0;i<list.length;i++) {
+            list[i].style.fill = '#ececec';
+        }
+    })
+
+    elem.addEventListener("click",function(event){
+        // API Call
+        getData(elem.getAttribute("name"));
+    })
+
+})
+
+// ZOOM EFFECT
+zoomEffect()
